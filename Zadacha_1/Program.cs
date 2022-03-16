6272c28984cce7b22b86375f7a40f133b2ac8ae2
Julia <jlomako@yandex.ru>
2022-03-16 15:28:45 +0300

    Задача вторая

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое целое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numA > numB)
+{
+        Console.WriteLine("Максимальное");
+}
+
+else 
+{
+    Console.WriteLine("Минимальоне");
+}
