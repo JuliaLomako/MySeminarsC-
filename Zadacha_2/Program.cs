235492e319303a37d4bfcaf8c84c0d8b04e07167
Julia <jlomako@yandex.ru>
2022-03-16 15:39:35 +0300

    Решение второй задачи

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите любое целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == number / number)
+{
+        Console.WriteLine("Число является чётным");
+}
+
+else 
+{
+    Console.WriteLine("Число является нечётным");
+}
