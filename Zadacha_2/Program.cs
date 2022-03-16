51cc6efa0d5595aacdea3f117004b70c7d40485b
Julia <jlomako@yandex.ru>
2022-03-16 18:34:40 +0300

    Две задачи готовы

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -1,12 +1,13 @@
-﻿Console.Write("Введите любое целое число ");
+﻿//Павел, добрый вечер! в настоящий момент в процессе решения))) 
+//мучаю уже несколько часов две задачи
+
+//Задача номер 3 (по списку)
+
+Console.Write("Введите любое целое число ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number == number / number)
-{
-        Console.WriteLine("Число является чётным");
-}
+if(number = number / number)
+Console.WriteLine("Число является чётным");
 
-else 
-{
-    Console.WriteLine("Число является нечётным");
-}
+else
+Console.WriteLine("Число является нечётным");
