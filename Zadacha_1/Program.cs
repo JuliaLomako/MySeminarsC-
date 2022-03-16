fe55d145de4e9d92f12aa85c8ef09b96fe7b5420
Julia <jlomako@yandex.ru>
2022-03-16 17:38:56 +0300

    Решение первой задачи

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -7,9 +7,15 @@ Console.Write("Введите второе целое число ");
 int numB = Convert.ToInt32(Console.ReadLine());
 
 int max = numA;
+int min = numB;
 
 if(numA > max) max = numA;
 if(numB > max) max = numB;
+if(numA < min) min = numA;
+if(numB < min) min = numB;
 
 Console.Write("Максимальное = ");
 Console.Write(max);
+
+Console.Write(" Минимальное = ");
+Console.Write(min);
