de4fdffab75498307b902d9758586c4ee9be832e
Julia <jlomako@yandex.ru>
2022-03-16 16:09:12 +0300

    Решение третей задачи

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача номер 2 (по списку)
+
+Console.Write("Введите первое целое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье целое число ");
+int numС = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if(numA > max) max = numA;
+if(numB > max) max = numB;
+if(numС > max) max = numС;
+
+Console.Write("Максимальное = ");
+Console.Write(max);
